a4c1ebba617043dfd43d894b88b4261263a02b9c
Bob Charlemagne Pierre <pbobc10@gmail.com>
2023-07-13 13:56:00 -0400

    change password ok

--- BSDevHRSystem/registration/views/views.py
+++ BSDevHRSystem/registration/views/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, HttpResponseRedirect
+from django.shortcuts import render, HttpResponseRedirect,redirect
 from django.urls import reverse
 from django.contrib.auth import authenticate,login,logout
-from django.http import HttpResponse
 from ..forms.user_form import LoginForm
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 # if no user is signed in, return to login page:
@@ -14,6 +15,7 @@ def index(request):
 
 def login_view(request):
     form = LoginForm()
+    message=''
     if request.method == 'POST':
         # Accessing username and password from form data
         username = request.POST['username']
@@ -23,25 +25,36 @@ def login_view(request):
         user = authenticate(request,username=username,password=password)
 
         # if user object is returned, log in and route to dashboard page:
-        if user:
-            login(request,user)
-            if user.last_login:
-                return HttpResponseRedirect(reverse('hrSystem:dashboard'))
+        if user is not None :
+           
+            if user.last_login is not None :
+                login(request,user)# create session
+                # Get the value of the "next" parameter from the request's GET data
+                next_url = request.GET.get('next')
+                if next_url is not None:
+                    return HttpResponseRedirect(next_url)
+                else:
+                    return HttpResponseRedirect(reverse('hrSystem:dashboard'))
             else:
                 # Redirect to change password page for first time login
-                return HttpResponseRedirect(reverse('accounts:change_password'))
+                login(request,user)# create session
+                force_password_change(user.id)
+                # Redirect to change password page for first time login
+                return redirect ('accounts:change_password')
+            
                 
         # otherwise, return login page again with new context
         else:
-            return render(request,'registration/login.html',{
-                'message':'Your username and password dind''t match',
-                'form':form,
-            })
+            message='username or password is incorrect'
+            
     #send user to login page
-    context = {'form':form}
+    context = {'form':form,'message':message}
     return render(request,'registration/login.html',context)
 
-
+def force_password_change(user_id):
+    user = User.objects.get(id=user_id)
+    user.force_password_change = True
+    user.save()   
 
 def logout_view(request):
     logout(request)
